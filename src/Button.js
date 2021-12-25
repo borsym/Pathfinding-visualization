ccfc8f9cb065c8fb540443157be5931024c51b49
Borsy Máté <52719798+borsym@users.noreply.github.com>
2021-12-25 16:28:50 +0100

    ui: comments removed

--- src/Button.js
+++ src/Button.js
@@ -1,15 +1,4 @@
 export default function Button(props) {
-  //   if (props.isVisualize === "true") {
-  //     return (
-  //   <button
-  //     className={`btn-navbar bg-cyan-600 hover:bg-cyan-500 hover:text-blue-800`}
-  //   >
-  //     {props.name}
-  //   </button>;
-  //     );
-  //   } else {
-  //     return <button className={`btn-navbar`}>{props.name}</button>;
-  //   }
   const buttonDesing =
     props.isVisualize === "true"
       ? `btn-navbar bg-cyan-600 hover:bg-cyan-500 hover:text-blue-800`
